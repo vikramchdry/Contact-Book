a94cb559db3cb4ebcca3fd68db5101ca328d7fac
Vikram Singh <ec1029231058@gmail.com>
2020-12-14 06:55:30 +0100

    second commit

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -14,33 +14,44 @@ function AddContact() {
     };
 
     return (
+
         <div className="card border-0 shadow">
-            <div className="card-header">
-                Add a Contact
-            </div>
+            <div className="card-header">Add a Contact</div>
             <div className="card-body">
                 <form onSubmit={(e) => createContact(e)}>
-                    <input type="text" className="form-group"
-                        placeholder="Enter Your Name" value={name} onChange={(e) => setName(e.target.value)} />
-                </form>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter Your Name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter Your Phone Number"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter Your E-mail Address"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
             </div>
-            <div className="card-body">
-                <form>
-                    <input type="text" className="form-group"
-                        placeholder="Enter Your Phone Number" value={phone} onChange={(e) => setPhone(e.target.value)} />
-                </form>
+                    <button className="btn btn-primary" type="submit">
+                        Create Contact
+            </button>
+          </form>
             </div>
-            <div className="card-body">
-                <form>
-                    <input type="text" className="form-group"
-                        placeholder="Enter Your E-mail Address" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <button className="btn btn-primary" type="submit" >Create Contact</button>
-                </form>
-            </div>
-
-
-
-        </div >
+        </div>
     );
 }
 
